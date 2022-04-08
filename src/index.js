2a6259abf6c24a8b1e3376f0748668d1846b3374
francamaradev <francamaradev@gmail.com>
2022-04-08 03:27:42 -0300

    feat: added css to dev environment and build (css not in build yet)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './style.css'
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <h1 className="text" id="yellow">Hello World</h1>,
   document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,33 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js')
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development'
 }
